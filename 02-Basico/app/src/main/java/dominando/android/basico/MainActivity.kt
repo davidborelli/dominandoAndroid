4bbe47b5f84c66b57bb67f42bdc9fcc24a778f98
David <david_borelli@live.com>
2020-01-07 00:13:57 -0300

    Término Capítulo 1

--- 02-Basico/app/src/main/java/dominando/android/basico/MainActivity.kt
+++ 02-Basico/app/src/main/java/dominando/android/basico/MainActivity.kt
@@ -2,11 +2,32 @@ package dominando.android.basico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Acessando SEM import kotlinx.android.synthetic.main.activity_main.* importado
+        val edtTexto = findViewById<EditText>(R.id.editTexto)
+        val button = findViewById<Button>(R.id.buttonToast)
+
+        button.setOnClickListener {
+            val texto = edtTexto.text.toString()
+            Toast.makeText(this, texto, Toast.LENGTH_SHORT).show()
+        }
+        */
+
+        /* Acessando COM import kotlinx.android.synthetic.main.activity_main.* importado  */
+        buttonToast.setOnClickListener {
+            val texto = editTexto.text.toString()
+            Toast.makeText(this, texto, Toast.LENGTH_SHORT).show()
+        }
     }
 }
